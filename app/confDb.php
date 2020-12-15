95da42f89fe8ad6b263317c93f7a8be5d5906a34
github.com/moonoise <monoise@gmail.com>
2020-12-15 09:21:55 +0700

    clear cached

--- app/confDb.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-date_default_timezone_set("Asia/bangkok");
-
-$db_host = "rid_Mariadb"; // Host name
-$db_username = "root"; // Mysql username
-$db_password = "root"; // Mysql password
-$db_name = "etraining"; // Database name
